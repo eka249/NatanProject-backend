2e304eeb2c35e33d79fb16a7b8e7e7728171709f
unknown <eka249@nyu.edu>
2020-03-01 22:59:20 -0800

    testing db

---
+++ app/models/orders_fact.rb
@@ -0,0 +1,2 @@
+class OrdersFact < ApplicationRecord
+end
---
+++ db/migrate/20200302005306_create_orders_facts.rb
@@ -0,0 +1,14 @@
+class CreateOrdersFacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders_facts do |t|
+      t.integer CustomerID
+      t.integer ProductID
+      t.integer EmployeeID
+      t.integer SupplierID
+      t.float Price
+      t.integer Quantity
+
+      t.timestamps
+    end
+  end
+end
