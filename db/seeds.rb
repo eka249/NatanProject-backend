82f2c4818bd0296c5c1cc2cce836942236d6fd35
unknown <eka249@nyu.edu>
2020-03-01 16:00:14 -0800

    additions to faker database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+    Employee.create(
+        LastName: Faker::Name.first_name,
+        FirstName: Faker::Name.last_name,
+        City: Faker::Address.city
+    )
+
+100.times do
+    Product.create(
+        ProductName: Faker::Food.ingredient,
+        SupplierID: Faker::Alphanumeric(number:8, min_alpha:6),
+        CategoryID: Faker::Number.number(leading_zero_number, digits: 3)
+
+
+    )
+
+30.times do
+    Supplier.create(
+        CompanyName: Faker::Company.name,
+        ContactName: Faker::Name,
+        City: Faker::Location.city,
+        Country: Faker::Location.country
+    )
