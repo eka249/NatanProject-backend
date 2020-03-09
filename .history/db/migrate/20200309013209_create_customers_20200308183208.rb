cb393a0c1c1ff328587a1c3f5f847f262482b98b
Ellyn <eka249@nyu.edu>
2020-03-08 18:35:20 -0700

    initial database created-will add on if necessary

--- .history/db/migrate/20200309013209_create_customers_20200308183208.rb
+++ .history/db/migrate/20200309013209_create_customers_20200308183208.rb
@@ -1,10 +1,7 @@
 class CreateCustomers < ActiveRecord::Migration[5.2]
   def change
     create_table :customers do |t|
-      t.integer :CustomerID
-      t.string :CompanyName
-      t.string :City
-      t.string :Country
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_005407) do
+ActiveRecord::Schema.define(version: 2020_03_09_013209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.integer "CustomerID"
+    t.string "CompanyName"
+    t.string "City"
+    t.string "Country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string "LastName"
     t.string "FirstName"
     t.string "City"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders_facts", force: :cascade do |t|
+    t.integer "CustomerID"
+    t.integer "ProductID"
+    t.integer "EmployeeID"
+    t.integer "SupplierID"
+    t.float "Price"
+    t.integer "Quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
